2219915a41369fed69d0847cf95faf00e7e38fcf
RafiZz <dribblerafis@gmail.com>
2018-04-24 18:26:15 +0500

    success 1.9.11

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+# sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn stop
+sudo gunicorn -b 0.0.0.0:8080 -c etc/gunicorn.conf hello:app
 # sudo /etc/init.d/mysql start
